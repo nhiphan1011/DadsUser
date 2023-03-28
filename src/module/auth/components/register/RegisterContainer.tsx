93b3c46875dac29d389465b20501264f89dd1eb6
NhiPhan <nhinhii1011@gmail.com>
2023-03-28 18:32:37 +0700

    update build

--- src/module/auth/components/register/RegisterContainer.tsx
+++ src/module/auth/components/register/RegisterContainer.tsx
@@ -3,22 +3,22 @@ import useGoogleAuth from "hooks/googleAuthHook";
 
 import RegisterView from "./RegisterView";
 
-import { NInitialState, registerUser, registerWithForm, selectAuth } from "module/auth";
+// import { NInitialState, registerUser, registerWithForm, selectAuth } from "module/auth";
 
 function RegisterContainer() {
   const dispatch = useAppDispatch();
 
   const { handleGoogleButton } = useGoogleAuth();
 
-  const onSubmitForm = (values: NInitialState.IREGISTER_INITIALSTATE) => {
-    dispatch(registerWithForm({ addInput: values }));
-  };
+  // const onSubmitForm = (values: NInitialState.IREGISTER_INITIALSTATE) => {
+  //   dispatch(registerWithForm({ addInput: values }));
+  // };
 
   const handleMetaSubmit = async () => {
     // dispatch(registerUser({ referralOther: undefined }));
   };
 
-  return <RegisterView formSubmit={onSubmitForm} metaSubmit={handleMetaSubmit} googleSubmit={handleGoogleButton} />;
+  // return <RegisterView formSubmit={onSubmitForm} metaSubmit={handleMetaSubmit} googleSubmit={handleGoogleButton} />;
 }
 
 export default RegisterContainer;
